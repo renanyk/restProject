c61a094c446ee2eab084b18dc10630f61e4d1c91
renanyk <renanyk78@hotmail.com>
2017-11-20 17:06:05 -0200

    Update produtos.php

--- FrontEnd/produtos.php
+++ FrontEnd/produtos.php
@@ -20,7 +20,7 @@
 
 <html>
     <head>
-        <title>Pagamento</title>  
+        <title>Planos</title>  
         <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
 
     </head>
